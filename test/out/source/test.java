3fc731dc56490e4e0fc93cde073326ea7952d9a9
Rascal0902 <geonho050902@gmail.com>
2025-05-13 21:14:30 +0900

    repo move

---
+++ test/out/source/test.java
@@ -0,0 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.awt.Frame; 
+import processing.awt.PSurfaceAWT; 
+import processing.core.PApplet; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+
+
+
+
+
+public class test extends PApplet {
+    public static void main(String args[]) {
+        PApplet.main("test");
+    }
+    public void settings() { size(500,500);}
+    public void setup() { background(0);}
+    public void draw() {}
+}
---
+++ test/test.pde
@@ -0,0 +1,11 @@
+import java.awt.Frame;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+public class test extends PApplet {
+    public static void main(String args[]) {
+        PApplet.main("test");
+    }
+    public void settings() { size(500,500);}
+    public void setup() { background(0);}
+    public void draw() {}
+}
